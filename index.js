5bd5e4f67e004d9ffc0a98404b121862789b372e
faraz141 <alamfaraz141@gmail.com>
2025-08-12 03:45:51 +0500

    create quiz-crud

--- index.js
+++ index.js
@@ -1,21 +1,24 @@
-require('dotenv').config()
-const cors = require('cors')
+require("dotenv").config();
+const cors = require("cors");
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-const authRoutes = require('./routes/userRoute')
+const authRoutes = require("./routes/userRoute");
+const quizRoutes = require("./routes/quizRoute");
 
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
-mongoose.connect(process.env.MONGO_URI)
-.then(()=>console.log('MongoDB connected successfully'))
-.catch(err=>console.error(err))
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB connected successfully"))
+  .catch((err) => console.error(err));
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.use('/auth', authRoutes)
+app.use("/auth", authRoutes);
+app.use("/api/quiz", quizRoutes);
 
-const PORT = process.env.PORT
-app.listen(PORT, ()=>console.log(`Server running on port ${PORT}`))
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
